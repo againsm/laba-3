69ef51b3dccb1e05016932b0c9baf729e6585dc4
Admin <Admin@HOME-PC>
2022-05-11 22:59:06 +0300

    Добавьте файлы проекта.

---
+++ laba-3/laba-3.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "1  " << ";" << "\n";
+
+    std::cout << "2 " << ";" << "\n";
+    int i = 19;
+    int in = 15;
+    int inn = 20;
+    std::cout << "Result i + in + inn = " << i + in + inn << ";" << "\n";
+    
+    float f = 10.10;
+    float fl;
+    float coc = 10.10;
+    float vosk = 230;
+    float sas = 30.50;
+    float honey = 550;
+    float Bott = 30.75;
+    std::cout << "3" << ";" << "\n";
+    std::cout << "Shopping list: = " << coc + vosk + sas + honey + Bott << ";" << "\n";
+   
+
+
+    int first = 25;
+    int second = 10;
+    std::cout << "4 " << ";" << "\n";
+    std::cout << "first = " << first << ";" << "\n";
+    std::cout << "second = " << second << ";" << "\n";
+    first = first + second;
+    second = first - second;
+    first = first - second;
+    std::cout << "first = " << first << ";" << "\n";
+    std::cout << "second = " << second << ";" << "\n";
+    
+
+
+    std::cout << "I eshe odin varik:" << ";" << "\n";
+    int a = 5;
+    int b = 10;
+    std::cout << "a = " << a << ";" << "\n";
+    std::cout << "b = " << b << ";" << "\n";
+    std::swap(a, b);
+    std::cout << "a = " << a << ";" << "\n";
+    std::cout << "b = " << b << ";" << "\n";
+    
+
+
+    std::cout << "5 " << ";" << "\n";
+    int alahakbar = 2147483647;
+    std::cout << alahakbar + 1 << ";" << "\n";
+    
+
+    std::cout << "6 " << ";" << "\n";
+    int a1 = 10;
+    int a2 = a1;
+    int a3 = a1;
+    printf("a1 = %d;\na2 = %d;\na3 = %d;\n", a1, a2, a3);
+    
+
+    double d = 666.999;
+    double dou;
+    char c = 'c';
+    char ch;
+    bool boo = true;
+    bool booo;
+}
+
+ 
